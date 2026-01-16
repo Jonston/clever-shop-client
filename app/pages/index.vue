ef60db5e5be02cfc1a37f79fd1b1a81fbaf16e5c
Eugene <jonston85@gmail.com>
2026-01-17 00:20:53 +0200

    Initial setup with Nuxt UI

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold mb-4">Welcome to Nuxt UI</h1>
+    <UButton>Click me</UButton>
+    <UCard class="mt-4">
+      <template #header>
+        <h3>Card Title</h3>
+      </template>
+      This is a card with Nuxt UI styles.
+    </UCard>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/ui'
+  ],
+  css: [
+    '~/assets/css/main.css'
+  ],
+  devtools: {
+    enabled: false
+  },
+  compatibilityDate: '2024-04-03',
+})
